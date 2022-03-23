27d4af133996e107c12f5f2966f8d4ec119de87b
ta <ta@MacBook-Pro-von-ta.local>
2022-03-23 12:48:18 +0100

    init

---
+++ src/main/java/com/ann/restCrypto/RestCryptoApplication.java
@@ -0,0 +1,13 @@
+package com.ann.restCrypto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestCryptoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestCryptoApplication.class, args);
+	}
+
+}
